a60e880981f2a6c03c0d6d24aa8799e4fe05d4a6
Lewis Scott <lewisscott0@gmail.com>
2023-09-11 23:58:13 +0000

    Fixed coverage job.

--- test/src/bus_test.cpp
+++ test/src/bus_test.cpp
@@ -1,6 +1,11 @@
 #include <gtest/gtest.h>
 
+#include <iostream>
+
+#include <neumann.hpp>
+
 TEST(ExpectCorrect, Succeed)
 {
+    std::cout << "Neumann " << Neumann::Version() << std::endl;
     EXPECT_EQ(true, true);
 }
